7598a218e28d3602431f4e39f87c1b44a33c48d9
Lucas deOliveira <ludeoliv@ie.ibm.com>
2017-03-27 13:45:01 +0100

    fixed #1

--- 2-JS-Basics/script.js
+++ 2-JS-Basics/script.js
@@ -190,3 +190,50 @@ if (calcJohn > calcFriend && calcJohn > calcAnotherFriend){
 } else {
   console.log("there is a draw");
 }
+
+
+
+///////////////////////////////////////
+// CODING CHALLENGE 2
+
+/*
+1. Create an array with some years where persons were born
+2. Create an empty array (just [] )
+3. Use a loop to fill the array with the ages of the persons
+4. Use another loop to log into the console whether each person is of full age (18 or older), as well as their age
+
+5. Finally, create a function called printFullAge which receives the array of years as an argument, executes the steps 2., 3. and 4. and returns an array of true/false boolean values: true if the person is of full age (>= 18 years) and false if not (< 18 years)
+6. Call the function with two different arrays and store the results in two variables: full_1 and full_2
+
+Example input:  [1965, 2008, 1992]
+Example output: [true, false, true]
+
+Hint: you can use a loop not only to read from an array, like y[i], but also to set values in an array, like y[i] = ... You can also use the specific array methods.
+*/
+
+var age = [2015,2016,2011,1990,2001,1986,1970,1974,2010,2013,1998,1994];
+//var empty = []
+var full_1 = [1965, 2008, 1992]
+var full_2 = [2015, 2016, 2011]
+
+console.log(printFullAge(full_1));
+console.log(printFullAge(full_2));
+
+function printFullAge(age){
+
+  var empty = [];
+  var ages = [];
+  for (var i=0 ; i < age.length; i++){
+    empty.push(age[i]);
+  }
+
+  for (var i = 0; i < empty.length; i++) {
+    if(2017 - empty[i] >= 18){
+      ages.push(true);
+    } else {
+      ages.push(false);
+    }
+  }
+
+  return ages;
+}
