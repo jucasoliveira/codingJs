4edcfa12ac70248e90d2263a0af1cce3dda305ca
Lucas deOliveira <ludeoliv@ie.ibm.com>
2017-03-31 10:35:53 +0100

    @Update

--- 4-DOM-Pig-Game/app.js
+++ 4-DOM-Pig-Game/app.js
@@ -13,16 +13,14 @@ var scores, roundScore, activePlayer;
 
 scores = [0,0];
 roundScore = 0;
-activePlayer = 1;
+activePlayer = 0;
 
-//dice = Math.floor(Math.random() * 6) + 1;
-
-//document.querySelector('#current-' + activePlayer).textContent = dice;
 document.querySelector('.dice').style.display = 'none';
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
 
-function btn(){
-
-}
 
 document.querySelector('.btn-roll').addEventListener('click', function(){
   //1. Random number
@@ -35,5 +33,25 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
 
 
   //3. Update the round score
+  if(dice !== 1){
+    //add score
+    roundScore += dice;
+    document.querySelector('#current-' + activePlayer).textContent = roundScore;
+
+
+  } else {
+    //Next player
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+
+    document.querySelector('.dice').style.display = 'none';
+
+  }
 
 });
