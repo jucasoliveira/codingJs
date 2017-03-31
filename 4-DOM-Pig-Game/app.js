5c9b1b85e13356a2aa657da78853bc5b9dbabc64
Lucas deOliveira <ludeoliv@ie.ibm.com>
2017-03-31 10:10:27 +0100

    Fixed #9 - Add event listener on roll dice

--- 4-DOM-Pig-Game/app.js
+++ 4-DOM-Pig-Game/app.js
@@ -8,3 +8,32 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+var scores, roundScore, activePlayer;
+
+scores = [0,0];
+roundScore = 0;
+activePlayer = 1;
+
+//dice = Math.floor(Math.random() * 6) + 1;
+
+//document.querySelector('#current-' + activePlayer).textContent = dice;
+document.querySelector('.dice').style.display = 'none';
+
+function btn(){
+
+}
+
+document.querySelector('.btn-roll').addEventListener('click', function(){
+  //1. Random number
+  var dice = Math.floor(Math.random() * 6) + 1;
+
+  //2. Display the result
+  var showDice = document.querySelector('.dice');
+  showDice.style.display = 'block';
+  showDice.src = 'dice-' + dice + '.png';
+
+
+  //3. Update the round score
+
+});
