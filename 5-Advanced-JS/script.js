9ce53f73a0d8d2900a963bdbeb04b7cf27fcf50b
Lucas deOliveira <ludeoliv@ie.ibm.com>
2017-04-05 15:01:09 +0100

    Fixes #25

---
+++ 5-Advanced-JS/script.js
@@ -0,0 +1,56 @@
+// Function Constructor
+
+/*
+var john = {
+  name : "John",
+  yearOfBirth : 1990,
+  job: "Teacher"
+}
+*/
+/*
+var Person = function (name, yearOfBirth, job){
+  this.name = name;
+  this.yearOfBirth = yearOfBirth;
+  this.job = job;
+
+}
+
+Person.prototype.calculateAge = function () {
+  console.log(2016 - this.yearOfBirth);
+}
+
+Person.prototype.lastname = 'Smith';
+
+var john = new Person("John", 1990 , "teacher");
+var jane = new Person ('Jane', 1969, 'Designer');
+var mark = new Person ('Mark', 1948, 'retired');
+
+john.calculateAge();
+jane.calculateAge();
+mark.calculateAge();
+
+console.log(john.lastname);
+*/
+
+
+// Object.create
+/*
+var personProto = {
+    calculateAge : function() {
+      console.log(2016 - this.yearOfBirth);
+    }
+}
+
+var john = Object.create(personProto);
+
+john.name = 'John';
+john.yearOfBirth = 1990;
+john.job = 'teacher';
+
+var jane = Object.create(personProto,
+  {
+    name:{ value : 'Jane'},
+    yearOfBirth: {value:1969},
+    job: {value: 'designer'}
+  })
+*/
