c09463d088edbbbbf3f08e1678004a53d8aac5ad
Lucas deOliveira <ludeoliv@ie.ibm.com>
2017-04-11 14:22:34 +0100

    Fixed #31

--- 5-Advanced-JS/script.js
+++ 5-Advanced-JS/script.js
@@ -227,10 +227,36 @@ var emily = {
 
 //Method borrowing
 //john.presentation.call(emily,'friendly','afternoon');
-
+/*
 var johnFriendly = john.presentation.bind(john, 'friendly');
-
+var emilyFriendly = john.presentation.bind(emily, 'formal');
 johnFriendly('morning');
 johnFriendly('night');
 
+emilyFriendly('afternoon');
+
 //john.presentation.apply(emily, ['friendly','afternoon']);
+
+var years = [1990, 1965, 1937, 2005 , 1998];
+
+function arrayCalc(arr , fn){
+  var arrRes = [];
+  for (var i = 0; i < arr.length; i++) {
+    arrRes.push(fn(arr[i]));
+  }
+  return arrRes;
+}
+
+function calculateAge(el) {
+  return 2016 - el;
+}
+
+function isFullAge(limit, el){
+  return el >= limit;
+}
+
+var ages = arrayCalc(years,calculateAge);
+var fullJapan = arrayCalc(ages, isFullAge.bind(this,20));
+console.log(ages);
+console.log(fullJapan);
+*/
