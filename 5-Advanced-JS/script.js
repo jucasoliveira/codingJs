b123e2d97736e8dc20db619eba5dd419b81c8801
Lucas deOliveira <ludeoliv@ie.ibm.com>
2017-04-18 11:03:17 +0100

    Fixed #33

--- 5-Advanced-JS/script.js
+++ 5-Advanced-JS/script.js
@@ -260,3 +260,46 @@ var fullJapan = arrayCalc(ages, isFullAge.bind(this,20));
 console.log(ages);
 console.log(fullJapan);
 */
+
+
+
+/////////////////////////////
+// CODING CHALLENGE
+
+
+/*
+--- Let's build a fun quiz game in the console! ---
+
+1. Build a function constructor called Question to describe a question. A question should include:
+a) question itself
+b) the answers from which the player can choose the correct one (choose an adequate data structure here, array, object, etc.)
+c) correct answer (I would use a number for this)
+
+2. Create a couple of questions using the constructor
+
+3. Store them all inside an array
+
+4. Select one random question and log it on the console, together with the possible answers (each question should have a number) (Hint: write a method for the Question objects for this task).
+
+5. Use the 'prompt' function to ask the user for the correct answer. The user should input the number of the correct answer such as you displayed it on Task 4.
+
+6. Check if the answer is correct and print to the console whether the answer is correct ot nor (Hint: write another method for this).
+
+7. Suppose this code would be a plugin for other programmers to use in their code. So make sure that all your code is private and doesn't interfere with the other programmers code (Hint: we learned a special technique to do exactly that).
+*/
+
+function Question () {
+  var enunciate = ["Whos the best on Coding: \n 1. Lucas \n 2. Matheus \n 3. Pedro","Is Javascript the colest language in the world? \n 0. yes \n 1. no"];
+  var correctAnswer = [1,0];
+  var setEverything = Math.floor(Math.random() * enunciate.length);
+
+  console.log(enunciate[setEverything]);
+  var getAnswer = prompt("Give me answer");
+  if (correctAnswer[setEverything] == getAnswer) {
+    console.log("Acerto miserarvi");
+  } else{
+    console.log("Errrooooou!");
+  }
+}
+
+Question();
